678885e46447c22b2f120185c284f3385dcbfa7c
Luis Santos <lfsantos2008@Luiss-MacBook-Pro.local>
2019-10-25 00:52:39 -0500

    finishing SignInVC and getting firebase signin to work

---
+++ AustinWeirdAutos/Controller/SignInViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  AustinWeirdAutos
+//
+//  Created by Luis Santos on 10/11/19.
+//  Copyright © 2019 Luis Santos. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
+import SwiftKeychainWrapper
+
+class SignInViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+//    override func viewDidAppear(_ animated: Bool) {
+//
+//        if let _ = KeychainWrapper.standard.string(forKey: KEY_UID) {
+//
+//            performSegue(withIdentifier: "toFeedVC", sender: nil)
+//        }
+//
+//    }
+
+    @IBAction func loginTapped(_ sender: Any) {
+        
+        // if one or the other are empty we're just doing nothing
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                
+                if error != nil {  // Can't signIn, need to create user or handle error
+                    
+                    Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                        
+                        if error != nil { //Couldn't create a new user due to badpassword, bad email, etc...
+                            
+                            //TODO: check error and display them to user
+                            print("LUIS: Unable to Authinticate with FireBase")
+                            
+                            
+                        } else { // Sucessfully created a new user
+                            
+                            print("LUIS: Successfully authenticated with Firebase")
+                            
+                            if let authData = user {
+                                
+                                let userData = ["provider": authData.user.providerID]
+                                self.completeSignIn(id: authData.user.uid, userData: userData)
+                            }
+                        }
+                    })
+
+                    
+                    
+                    
+                    
+                    
+                } else { // Successfully Authenticated with Firebase
+                    
+                    //NO errors, email and password match with firebase
+                    print("LUIS: Email user authenticated with Firebase")
+                    
+                    if let authData = user {
+                        let userData = ["provider": authData.user.providerID]
+                        self.completeSignIn(id: authData.user.uid, userData: userData)
+                    }
+                    
+                    
+                    
+                }
+                
+            })
+            
+            
+            
+            
+        }
+    }
+    
+    
+    func completeSignIn(id: String, userData: Dictionary<String, String>){
+        
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
+        
+        KeychainWrapper.standard.set(id, forKey: "uid")
+        
+        passwordTextField.text = ""
+        
+        //performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
+    }
+        
+    
+}
+
---
+++ AustinWeirdAutos/Services/DataService.swift
@@ -0,0 +1,57 @@
+//
+//  DataService.swift
+//  AustinWeirdAutos
+//
+//  Created by Luis Santos on 10/22/19.
+//  Copyright © 2019 Luis Santos. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import SwiftKeychainWrapper
+
+let DB_BASE = Database.database().reference()
+let STORAGE_BASE = Storage.storage().reference()
+
+class DataService {
+    
+    static let ds = DataService()
+    
+    //DB references
+    private var _REF_BASE = DB_BASE
+    private var _REF_POSTS = DB_BASE.child("posts")
+    private var _REF_USERS = DB_BASE.child("users")
+    
+    //Storage reference
+    private var _REF_WEIRD_IMAGES = STORAGE_BASE.child("weird-pics")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    var REF_POSTS: DatabaseReference {
+        return _REF_POSTS
+    }
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    
+    var REF_USER_CURRENT: DatabaseReference {
+        let uid = KeychainWrapper.standard.string(forKey: "uid")
+        let user = REF_USERS.child(uid!)
+        return user
+    }
+    
+    var REF_WEIRD_IMAGES: StorageReference {
+        return _REF_WEIRD_IMAGES
+    }
+    
+    
+    
+    func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
+        
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+    
+    
+    
+}
