0a2d6210d0d5a29bab6ba3097f87e5267b8ddaf5
Luis Santos <lfsantos2008@Luiss-MacBook-Pro.local>
2019-11-14 20:01:52 -0600

    Finishing Feed View Controller

---
+++ AustinWeirdAutos/View/FeedPostCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedPostCell.swift
+//  AustinWeirdAutos
+//
+//  Created by Luis Santos on 11/2/19.
+//  Copyright © 2019 Luis Santos. All rights reserved.
+//
+
+import UIKit
+
+class FeedPostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
