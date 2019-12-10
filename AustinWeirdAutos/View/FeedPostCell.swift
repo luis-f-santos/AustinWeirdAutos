dbbc9566e41f0c4e94639c8bdaf003a3f4f2daa2
Luis Santos <lfsantos2008@Luiss-MacBook-Pro.local>
2019-12-10 00:20:12 -0600

    Fixing bugs for Scrollview inside FeedPostCell. Everyting should be working, need to edge case test

--- AustinWeirdAutos/View/FeedPostCell.swift
+++ AustinWeirdAutos/View/FeedPostCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class FeedPostCell: UITableViewCell {
+class FeedPostCell: UITableViewCell, UIScrollViewDelegate {
     
     
     @IBOutlet weak var makeImage: UIImageView!
@@ -21,14 +21,29 @@ class FeedPostCell: UITableViewCell {
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var numLikesLbl: UILabel!
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
     var post: Post!
     
-    var scrollView: UIScrollView!
+    var defaultImageView: UIImageView!
     
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        scrollView.delegate = self
+    }
+    
+//    func prepareForReuse() {
+//        <#code#>
+//    }
+
     
     func configureFeedCell(post: Post) {
         
-        scrollView.add
+        
         
         self.post =  post
         vehicleLbl.text  = "\(post.year) \(post.make) \(post.model)"
@@ -36,46 +51,113 @@ class FeedPostCell: UITableViewCell {
         descriptionTextView.text = post.description
         numLikesLbl.text = "\(post.likes)"
         postImage.image = UIImage (named: "red_tesla")  //Default image
-     
-        //URLSession.shared.downloadTask(with: URL(string: post.imageURLs[0] )!, completionHandler: <#T##(URL?, URLResponse?, Error?) -> Void#>)
         
+        
+        //scrollView = UIScrollView()
+        var defaultImageFrame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        defaultImageFrame.size = scrollView.frame.size
+        defaultImageView = UIImageView(frame: defaultImageFrame)
+        defaultImageView.contentMode = UIViewContentMode.scaleToFill
+        defaultImageView.clipsToBounds = true
+        defaultImageView.image = UIImage(named: "red_tesla")
+        
+        //scrollView.addSubview(defaultImageView)
+
+        //reset resuseable cell
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = 0
+        
+        let subviews = self.scrollView.subviews
+        for subview in subviews{
+            subview.removeFromSuperview()
+        }
+     
         if post.imageURLs.count > 0 {
             
-            if let img = FeedViewController.imageCache.object(forKey: post.imageURLs[0] as NSString) {
-                
-                print("Using imgage from Cache: \(post.imageURLs[0])")
-                self.postImage.image = img
-                
-            } else {
+            pageControl.numberOfPages = post.imageURLs.count
+
             
-                print("url to download: \(post.imageURLs[0])")
-                let ref = Storage.storage().reference(forURL: post.imageURLs[0])
+            if(post.imageURLs.count == 1){
+                pageControl.isHidden = true
+            }
+            else {
+                pageControl.isHidden = false
+            }
             
-                //maxSize = 2Mb
-                ref.getData(maxSize: 2 * 1024 * 1024, completion: { (data, error) in
+            
+            for index in 0..<post.imageURLs.count {
+                
+            
+                if let img = FeedViewController.imageCache.object(forKey: post.imageURLs[index] as NSString) {
                     
-                    if error != nil {
-                        print("LUIS: Unable to download image from FirebaseStorage")
-                    } else {
+                    print("Using imgage from Cache: \(post.imageURLs[index])")
+                    
+                    let imageView = UIImageView()
+                    imageView.contentMode = .scaleAspectFit
+                    imageView.clipsToBounds = true
+                    imageView.image = img
+                    
+                    let xPos = CGFloat(index) * self.bounds.size.width
+                    
+                    imageView.frame = CGRect(x: xPos, y: 0, width: self.frame.size.width, height: self.scrollView.frame.size.height)
+                    
+                    scrollView.contentSize.width = self.frame.size.width*CGFloat(index+1)
+                    
+                    scrollView.addSubview(imageView)
+                    //self.postImage.image = img
+                    
+                } else {
+                
+                    print("url to download: \(post.imageURLs[0])")
+                    let ref = Storage.storage().reference(forURL: post.imageURLs[index])
+                
+                    //maxSize = 2Mb
+                    ref.getData(maxSize: 2 * 1024 * 1024, completion: { (data, error) in
                         
-                        print("LUIS: Successfull Downloaded from firebase storage")
-                        
-                        if let imgData = data {
-                            if let img = UIImage(data: imgData){
-                                self.postImage.image = img
-                                FeedViewController.imageCache.setObject(img, forKey: post.imageURLs[0] as NSString)
+                        if error != nil {
+                            print("LUIS: Unable to download image from FirebaseStorage")
+                        } else {
+                            print("LUIS: Successfull Downloaded from firebase storage")
+                            
+                            if let imgData = data {
+                                if let img = UIImage(data: imgData){
+                                    
+                                    let imageView = UIImageView()
+                                    imageView.contentMode = .scaleAspectFit
+                                    imageView.clipsToBounds = true
+                                    imageView.image = img
+                                    
+                                    let xPos = CGFloat(index) * self.bounds.size.width
+                                    
+                                    imageView.frame = CGRect(x: xPos, y: 0, width: self.frame.size.width, height: self.scrollView.frame.size.height)
+                                    
+                                    self.scrollView.contentSize.width = self.frame.size.width*CGFloat(index+1)
+                                    
+                                    self.scrollView.addSubview(imageView)
+                                    //self.postImage.image = img
+                                    FeedViewController.imageCache.setObject(img, forKey: post.imageURLs[index] as NSString)
+                                }
                             }
                         }
-                    }
-                })
+                    })
+                    
+                }
             }
         }
         
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page  = scrollView.contentOffset.x/scrollView.frame.size.width
+        pageControl.currentPage = Int(page)
+        
+        
     }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+    }
+//9D5BAB5C-176D-4035-B235-10BC854AAC0D
+//    "gs://austin-weird-autos.appspot.com/weird-pics/9D5BAB5C-176D-4035-B235-10BC854AAC0D"
 
 }
